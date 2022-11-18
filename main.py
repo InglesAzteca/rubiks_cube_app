4555ee27600ef803ad4a125fdf50d0e6fb682c5e
InglesAzteca <117683682+InglesAzteca@users.noreply.github.com>
2022-11-18 23:50:56 +0000

    styling

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import tkinter
 import customtkinter
 
-customtkinter.set_appearance_mode(
-    "Dark")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme(
-    "blue")  # Themes: "blue" (standard), "green", "dark-blue"
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
 class RubiksApp(customtkinter.CTk):
